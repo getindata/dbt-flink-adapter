d7fc11cbad44c142e39b59065bcbbb9d6ddf0f9b
dormi330 <dormi330@gmail.com>
2023-02-09 16:04:41 +0800

    support computed / metadata column

--- dbt/include/flink/macros/materializations/sources/source.sql
+++ dbt/include/flink/macros/materializations/sources/source.sql
@@ -8,9 +8,14 @@
 {% set type = node.config.get('type', None) %}
 {% set table_column_ids = node.columns.keys() %}
 CREATE TABLE IF NOT EXISTS {{ node.identifier }} {% if type %}/** mode('{{type}}')*/{% endif %} (
-{% for column_id in table_column_ids %} `{{ node.columns[column_id]["name"] }}` {{ node.columns[column_id]["data_type"] }}{% if not loop.last %},{% endif %}
+{% for column_id in table_column_ids %}
+    {%- if node.columns[column_id]["column_type"] == 'metadata' %} `{{ node.columns[column_id]["name"] }}` {{ node.columns[column_id]["data_type"] }} METADATA {% if node.columns[column_id]["expression"] %} FROM '{{node.columns[column_id]["expression"]}}' {% endif %}
+    {%- elif node.columns[column_id]["column_type"] == 'computed' %} `{{ node.columns[column_id]["name"] }}` AS {{ node.columns[column_id]["expression"] }}
+    {%- else %} `{{ node.columns[column_id]["name"] }}` {{ node.columns[column_id]["data_type"] }}
+    {%- endif %}
+    {%- if not loop.last %},{% endif %}
 {% endfor %}
-{% if watermark_properties %}, WATERMARK FOR {{ watermark_properties['column']}} AS {{ watermark_properties['strategy']}} {% endif %}
+{%- if watermark_properties %}, WATERMARK FOR {{ watermark_properties['column']}} AS {{ watermark_properties['strategy']}} {% endif %}
 )
 with (
 {% for property_name in connector_properties %} '{{ property_name }}' = '{{ connector_properties[property_name] }}'{% if not loop.last %},{% endif %}
